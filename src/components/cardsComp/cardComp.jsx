b10dc527ed3858c27a59f01575bb35b7bdff0192
steeven-louk <steevenlouk.sl@gmail.com>
2022-08-01 11:46:39 +0200

    mise en place temporaire du composant card

---
+++ src/components/cardsComp/cardComp.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+import './styles/style.scss';
+
+export const CardComp = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="card">
+          <div className="card-img-top">
+            <img src="https://place-hold.it/250" alt="" srcset="" />
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">bulbizar</h4>
+            <div className="card-desc">petite desc</div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-img-top">
+            <img src="https://place-hold.it/250" alt="" srcset="" />
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">bulbizar</h4>
+            <div className="card-desc">petite desc</div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-img-top">
+            <img src="https://place-hold.it/250" alt="" srcset="" />
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">bulbizar</h4>
+            <div className="card-desc">petite desc</div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-img-top">
+            <img src="https://place-hold.it/250" alt="" srcset="" />
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">bulbizar</h4>
+            <div className="card-desc">petite desc</div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-img-top">
+            <img src="https://place-hold.it/250" alt="" srcset="" />
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">bulbizar</h4>
+            <div className="card-desc">petite desc</div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-img-top">
+            <img src="https://place-hold.it/250" alt="" srcset="" />
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">bulbizar</h4>
+            <div className="card-desc">petite desc</div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card-img-top">
+            <img src="https://place-hold.it/250" alt="" srcset="" />
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">bulbizar</h4>
+            <div className="card-desc">petite desc</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { CardComp } from '../../components/cardsComp/cardComp'
 
 export const Home = () => {
   return (
     <>
         <div className="container">
-            
+            <CardComp />
         </div>
     </>
   )
